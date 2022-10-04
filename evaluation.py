2dc1ec89dc98995c88cd28d676863d5ad1f18676
Aryan Yousefi <aryan.yousefi95@gmail.com>
2022-10-04 13:33:58 -0400

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -16,13 +16,12 @@ from torchvision.transforms.functional import InterpolationMode
 import evaluation
 import utils
 from evaluation import compute_emotional_alignment
-from data import DataLoader, ImageField, EmotionField, TextField, RawField, Merge #AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+from data import DataLoader, ImageField, EmotionField, TextField, RawField, Merge
 from models import Captioner
 from models import clip
 from models.blip import blip_feature_extractor
 from data import ArtEmis, ArtEmisDetectionsField
-from six.moves import cPickle, range#BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
-#from simpletransformers.classification import ClassificationModel
+from six.moves import cPickle, range
 
 random.seed(1234)
 torch.manual_seed(1234)
@@ -62,66 +61,40 @@ def unpickle_data(file_name, python2_to_3=False):
     in_file.close()
     
     
-def flatten(l):#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-    if isinstance(l, list):#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-        for v in l:#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-            yield from flatten(v)#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-    else:#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-        yield l#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+def flatten(l):
+    if isinstance(l, list):
+        for v in l:
+            yield from flatten(v)
+    else:
+        yield l
 
         
-def evaluate_emoalign(ground_truth, captions, split):#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBB
-    wiki_art_img_dir = '/home/ayousefi/wikiart'
-    references_file = '/home/ayousefi/artemis-master/artemis/preprocessed_for_deep_net/artemis_gt_references_grouped.pkl'
-    text2emo_path = '/home/ayousefi/artemis-master/artemis/classifiers/txt_to_emotion_lstm_based/best_model.pt'
-    text2emo_bert_path = '/home/ayousefi/projects/def-kpassi/ayousefi/txt_to_emotion_bert_based/outputs/best_model/pytorch_model.bin'
-    vocab_path = '/home/ayousefi/artemis-master/artemis/preprocessed_for_deep_net/vocabulary.pkl'    
-    gt_data = next(unpickle_data(references_file))#BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+def evaluate_emoalign(ground_truth, captions, split):
+    wiki_art_img_dir = '/dir'
+    references_file = '/dir'
+    text2emo_path = '/dir'
+    text2emo_bert_path = '/dir'
+    vocab_path = '/dir'
+    gt_data = next(unpickle_data(references_file))
 
-    train_utters = gt_data['train']['references_pre_vocab']#BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
-    train_utters = list(itertools.chain(*train_utters))  #BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
-    print('Training Utterances', len(train_utters))#BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
-    unique_train_utters = set(train_utters)#BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
-    print('Unique Training Utterances', len(unique_train_utters))#BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+    train_utters = gt_data['train']['references_pre_vocab']
+    train_utters = list(itertools.chain(*train_utters))
+    print('Training Utterances', len(train_utters))
+    unique_train_utters = set(train_utters)
+    print('Unique Training Utterances', len(unique_train_utters))
     
     gt_data = gt_data[split]
     print('Images Captioned', len(gt_data))
     for data in gt_data:
-        gt_data['path']= "/home/ayousefi/wikiart" + '/' + gt_data['art_style'] + '/' + gt_data ['painting'] + '.jpg'
+        gt_data['path']= "/dir" + '/' + gt_data['art_style'] + '/' + gt_data ['painting'] + '.jpg'
     
     txt2emo_clf = torch.load(text2emo_path, map_location=device)
-    #args = {'reprocess_input_data': True, 
-    #    'overwrite_output_dir': True,
-    #    'fp16': False,
-    #    'n_gpu': 4,
-    #    'save_model_every_epoch': False,
-    #    'evaluate_during_training': True,
-    #    'num_train_epochs': 50,
-    #    'min_frequency': 5,
-    #    'train_batch_size': 128,
-    #   }
-    #model = ClassificationModel('bert', '/home/ayousefi/artemis-master/bert-base-uncased', num_labels=9, args=args)
-    #txt2emo_clf = model.load_state_dict(torch.load(text2emo_bert_path, map_location='cpu'))
-    #_logger.info('GT_DATA:')
-    #_logger.info(gt_data)
-    #_logger.info('CAPTIONS:')
-    #_logger.info(captions)
     txt2emo_vocab = load_vocabulary(vocab_path)
     print('vocab size', len(txt2emo_vocab))
 
-#    for caption in captions:  # you might have sampled under several sampling configurations
-    merged = pd.merge(gt_data, captions)#BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
-    #_logger.info('MERGED:')
-    #_logger.info(merged)
-        #merged = pd.merge(ground_truth, captions)  # this ensures proper order of captions to gt (via accessing merged.captions)
+    merged = pd.merge(gt_data, captions)
     hypothesis = merged.caption # i.e., use references that do not have <UNK>
     ref_emotions = merged.emotion
-    #_logger.info(hypothesis)
-    #_logger.info("MERGED:")
-    #_logger.info(merged)#BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
-    #_logger.info("HYPOTHESIS:")
-    #_logger.info(hypothesis)#BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
-    #_logger.info(ref_emotions)#BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
     emoalign_score = compute_emotional_alignment(hypothesis, ref_emotions, txt2emo_clf, txt2emo_vocab, device=device)
     
     _logger.info('EMOTIONAL ALIGNMENT SCORE: ')
@@ -139,10 +112,10 @@ def evaluate_metrics(model, dataloader, text_field, emotion_encoder):
     paths = {}
     gen = {}
     gts = {}
-    captions = {'path':[], 'caption':[]}#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-    ground_truth = {'path':[], 'gtcaption':[]}#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-    gen_caps = pd.DataFrame(captions, columns=['path', 'caption'])#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-    gt_caps = pd.DataFrame(captions, columns=['path', 'gtcaption'])#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    captions = {'path':[], 'caption':[]}
+    ground_truth = {'path':[], 'gtcaption':[]}
+    gen_caps = pd.DataFrame(captions, columns=['path', 'caption'])
+    gt_caps = pd.DataFrame(captions, columns=['path', 'gtcaption'])
     header = 'Evaluation metrics:'
     with torch.no_grad():
         if args.feature_extractor == 'CLIP' or args.feature_extractor == 'BLIP':
@@ -158,27 +131,20 @@ def evaluate_metrics(model, dataloader, text_field, emotion_encoder):
                     enc_emotions = emotion_encoder(emotions)
                     enc_emotions = enc_emotions.unsqueeze(1).repeat(1, images.shape[1], 1)
                     images = torch.cat([images, enc_emotions], dim=-1)
-                #images = image_model(images)
                 text, _ = model.beam_search(images, beam_size=5, out_size=1)
                 caps_gen = text_field.decode(text)          
-    #            _logger.info(image_paths)
-    #            _logger.info(caps_gen)
-    #            _logger.info(emotions)
 
-                for i, (path_i, gts_i, gen_i) in enumerate(zip(image_paths, caps_gt, caps_gen)):#AAAAAAAAAAAAAAAAAAAAAAAAAAA
+                for i, (path_i, gts_i, gen_i) in enumerate(zip(image_paths, caps_gt, caps_gen)):
                     paths['%d_%d' % (it, i)] = path_i
                     gen['%d_%d' % (it, i)] = [gen_i, ]
                     gts['%d_%d' % (it, i)] = gts_i
-                    _logger.info(path_i)#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                    _logger.info(gen_i)#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                    #_logger.info(gts_i)#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                    #_logger.info(emo_i)#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                    #actual_path = "/home/ayousefi/wikiart"+path_i+".jpg"
-                    gen_caps = gen_caps.append({'path': path_i, 'caption': gen_i}, ignore_index=True)#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                    gt_caps = gt_caps.append({'path': path_i, 'gtcaption': gts_i}, ignore_index=True)#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+                    _logger.info(path_i)
+                    _logger.info(gen_i)
+                    gen_caps = gen_caps.append({'path': path_i, 'caption': gen_i}, ignore_index=True)
+                    gt_caps = gt_caps.append({'path': path_i, 'gtcaption': gts_i}, ignore_index=True)
                     
         elif args.feature_extractor == 'ResNet101':
-            for it, (detections, captions_emotions) in enumerate(iter(metric_logger.log_every(dataloader, _print_freq, header))):#AAAAAAAA
+            for it, (detections, captions_emotions) in enumerate(iter(metric_logger.log_every(dataloader, _print_freq, header))):
                 detections = detections.to(device)
                 caps_gt, emotions = captions_emotions
                 if emotion_encoder is not None:
@@ -189,18 +155,14 @@ def evaluate_metrics(model, dataloader, text_field, emotion_encoder):
                     enc_emotions = emotion_encoder(emotions)
                     enc_emotions = enc_emotions.unsqueeze(1).repeat(1, detections.shape[1], 1)
                     detections = torch.cat([detections, enc_emotions], dim=-1)
-                #images = image_model(images)
                 text, _ = model.beam_search(detections, beam_size=5, out_size=1)
                 caps_gen = text_field.decode(text)          
-    #            _logger.info(image_paths)
-    #            _logger.info(caps_gen)
-    #            _logger.info(emotions)
 
                 for i, (gts_i, gen_i) in enumerate(zip(caps_gt, caps_gen)):
                     gen['%d_%d' % (it, i)] = [gen_i, ]
                     gts['%d_%d' % (it, i)] = gts_i
-                    gen_caps = gen_caps.append({'path': gts_i, 'caption': gen_i}, ignore_index=True)#AAAWRONGWONRONORNEGONEOQKFOEKRFOQAEMKC
-                    gt_caps = gt_caps.append({'path': gen_i, 'gtcaption': gts_i}, ignore_index=True)#ANKSDVNOAWEOFKQWPOEKFOQPWKEFOPQWKEOQKWE
+                    gen_caps = gen_caps.append({'path': gts_i, 'caption': gen_i}, ignore_index=True)
+                    gt_caps = gt_caps.append({'path': gen_i, 'gtcaption': gts_i}, ignore_index=True)
                
     gts = evaluation.PTBTokenizer.tokenize(gts)
     gen = evaluation.PTBTokenizer.tokenize(gen)
@@ -210,7 +172,7 @@ def evaluate_metrics(model, dataloader, text_field, emotion_encoder):
     #_logger.info('GEN_CAPS:')
     #_logger.info(gen_caps)
     #_logger.info(gt_caps)
-    return scores, gt_caps, gen_caps #AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    return scores, gt_caps, gen_caps
 
 
 if __name__ == '__main__':
@@ -250,7 +212,7 @@ if __name__ == '__main__':
         image_field = ImageField(transform=transform)
     
     if args.feature_extractor == 'BLIP':
-        blip_model = blip_feature_extractor(pretrained='/home/ayousefi/projects/def-kpassi/ayousefi/blip_model_large.pth', image_size=224, vit='large')
+        blip_model = blip_feature_extractor(pretrained='/dir/blip_model_large.pth', image_size=224, vit='large')
         image_model = blip_model
         vision_width_large = 1024
         image_size = 224
@@ -266,7 +228,6 @@ if __name__ == '__main__':
         image_model = clip_model.visual
         image_model.forward = image_model.intermediate_features
         image_field = ArtEmisDetectionsField(detections_path=args.features_path, max_detections=50)
-    #args.image_dim = image_model.embed_dim
 
     # Pipeline for text
     text_field = TextField()
@@ -274,7 +235,7 @@ if __name__ == '__main__':
         'amusement', 'awe', 'contentment', 'excitement', 
         'anger', 'disgust', 'fear', 'sadness', 'something else'
         ]
-    emotion_field = EmotionField(emotions=emotions)#AAAAAAAAAAAAAAAAAAAA
+    emotion_field = EmotionField(emotions=emotions)
     
     emotion_dim = 0
     emotion_encoder = None
@@ -330,20 +291,15 @@ if __name__ == '__main__':
         
     # Validation captions
     _logger.info('Validation set')
-    val_scores, val_gt_caps, val_gen_caps = evaluate_metrics(model, dict_dataloader_val, text_field, emotion_encoder)#AAAAAAAA
+    val_scores, val_gt_caps, val_gen_caps = evaluate_metrics(model, dict_dataloader_val, text_field, emotion_encoder)
     _logger.info(f'Validation scores {val_scores}')
-    val_emo_score = evaluate_emoalign(val_gt_caps, val_gen_caps, 'val')#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA YOU CAN REMOVE VAL_GT_CAPS
-    _logger.info('Validation Emo-Align Score:')#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-    _logger.info(val_emo_score)#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-    #_logger.info('VAL_GEN_CAPS')
-    #_logger.info(val_gen_caps)
+    val_emo_score = evaluate_emoalign(val_gt_caps, val_gen_caps, 'val')
+    _logger.info('Validation Emo-Align Score:')
+    _logger.info(val_emo_score)
 
     # Test captions
     _logger.info('Test set')
     test_scores, test_gt_caps, test_gen_caps = evaluate_metrics(model, dict_dataloader_test, text_field, emotion_encoder)#AAAA
     _logger.info(f'Test scores {test_scores}')
-    test_emo_score = evaluate_emoalign(test_gt_caps, test_gen_caps, 'test')#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-    _logger.info('Test Emo-Align Score:')#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-    #_logger.info(test_emo_score)#AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA    
-    #_logger.info('TEST_GEN_CAPS')
-    #_logger.info(test_gen_caps)
+    test_emo_score = evaluate_emoalign(test_gt_caps, test_gen_caps, 'test')
+    _logger.info('Test Emo-Align Score:')
